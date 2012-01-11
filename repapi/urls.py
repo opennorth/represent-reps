6c151cfd34f8a9eb8b3a66d55872f67af32587de
Michael Mulley <michael@michaelmulley.com>
2012-01-10 23:51:07 -0500

    Another boundary->boundaries update

--- repapi/urls.py
+++ repapi/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls.defaults import patterns, include, url
 from repapi.views import *
 
 urlpatterns = patterns('',
-    url('^representative/$', RepresentativeListView.as_view()),
-    url('^(?P<district>boundary/[\w_-]+/[\w_-]+/)representatives/', RepresentativeListView.as_view()),
+    url('^representatives/$', RepresentativeListView.as_view()),
+    url('^(?P<district>boundaries/[\w_-]+/[\w_-]+/)representatives/', RepresentativeListView.as_view()),
 )
