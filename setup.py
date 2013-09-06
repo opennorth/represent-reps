48d27c6b7023fea7d85e153d23848eeeabc3c68a
Michael Mulley <michael@michaelmulley.com>
2013-09-06 15:12:48 -0400

    Improve setup.py

--- setup.py
+++ setup.py
@@ -5,11 +5,19 @@ from setuptools import setup
 
 setup(
     name='represent-representatives',
+    description='A Web API for political representatives tied to geographical districts. Packaged as a Django app.',
+    url='https://github.com/rhymeswithcycle/represent-reps',
     packages=['representatives', 'representatives.management', 'representatives.management.commands'],
-    version='0.0.1',
+    version='0.1',
     install_requires=[
         'django-appconf',
         'django-jsonfield>=0.7.1',
         'python-dateutil',
+        'represent-boundaries',
     ],
+    classifiers = [
+        'Programming Language :: Python :: 2.7',
+        'License :: OSI Approved :: MIT License',
+        'Framework :: Django',
+    ]
 )
