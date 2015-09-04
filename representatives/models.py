4137d20e0a9bb029ecbedcf0d0df2f95fe71f6bf
James McKinney <james@slashpoundbang.com>
2015-09-03 20:24:31 -0400

    fix variable name

--- representatives/models.py
+++ representatives/models.py
@@ -186,9 +186,9 @@ class BaseRepresentativeSet(models.Model):
             else:
                 rep.boundary = boundary_url_to_name(boundary_url)
                 if not rep.district_name:
-                    rep.district_name = url_to_name[boundaries_url]
+                    rep.district_name = url_to_name[boundary_url]
                 if not rep.district_id:
-                    rep.district_id = url_to_id[boundaries_url]
+                    rep.district_id = url_to_id[boundary_url]
             rep.save()
 
         self.last_import_time = datetime.datetime.now()
